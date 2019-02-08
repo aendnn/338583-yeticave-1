20df9845ffaaf41c11898eeb6633612924e87ae9
Anna <aendnn9@gmail.com>
2019-02-08 22:27:40 +0200

    Шаблонизация проекта

---
+++ templates/navigation.php
@@ -0,0 +1,10 @@
+<nav class="nav">
+    <ul class="nav__list container">
+        <?php
+        foreach ($categories as $item): ?>
+            <li class="nav__item">
+                <a href="all-lots.html"><?=$item; ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</nav>
